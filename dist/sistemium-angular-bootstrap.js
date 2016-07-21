72e4e522c08bf95098c864a6b0f0c60a9f2c074b
Alexander Levin <alevin.ru@gmail.com>
2016-07-21 22:13:44 +0300

    merge dist

--- dist/sistemium-angular-bootstrap.js
+++ dist/sistemium-angular-bootstrap.js
@@ -92,6 +92,154 @@
 
 }());
 
+
+(function () {
+  angular.module('sistemiumBootstrap.services')
+    .service('sabNgTable', function (NgTableParams) {
+
+      var lastFindAllParams = {},
+        lastFindAllData = [],
+        totalCount = 0
+        ;
+
+      function ngTableToV4Params(params) {
+
+        var result = {
+          'x-page-size:': params && params.count() || 12,
+          'x-start-page:': params && params.page() || 1
+        };
+
+        if (params && params.sorting()) {
+          var sortBy = _.reduce(params.sorting(), function (res, dir, field) {
+            return res + ',' + (dir === 'desc' ? '-' : '') + field;
+          }, '').substr(1);
+          if (sortBy) {
+            result['x-order-by:'] = sortBy;
+          }
+        }
+
+        return result;
+
+      }
+
+      function getData (ctrl,model) {
+
+        return function ($defer, params) {
+
+          var v4Params = ngTableToV4Params(params);
+          var needCount = !totalCount ||
+              _.get(v4Params, 'searchFor:') !== _.get(lastFindAllParams, 'searchFor:')
+            ;
+          var countPromise;
+          var setPage;
+
+          if (needCount) {
+            countPromise = model.getCount(_.pick(v4Params, ['searchFor:', 'searchFields:'])).then(function (res) {
+              ctrl.ngTableParams.total(totalCount = res);
+              if (res < (params.page() - 1) * params.count()) {
+                v4Params['x-start-page:'] = 1;
+                setPage = 1;
+              }
+              return v4Params;
+            });
+            countPromise.catch(function (res) {
+              //ctrl.processServerError(res);
+              $defer.reject();
+            });
+          }
+
+          var dataPromiseOrNothing = function () {
+            var p = v4Params;
+            if (!_.matches(p)(ctrl.lastFindAllParams) || !_.matches(ctrl.lastFindAllParams)(p)) {
+              return model.findAll(p, {bypassCache: true})
+                .then(function (data) {
+                  if (setPage) {
+                    params.page(setPage);
+                  }
+                  lastFindAllParams = p;
+                  lastFindAllData = data;
+                  $defer.resolve(lastFindAllData);
+                }, function () {
+                  $defer.reject();
+                });
+            } else {
+              if (setPage) {
+                params.page(setPage);
+              }
+              $defer.resolve(lastFindAllData);
+            }
+          };
+
+          if (countPromise) {
+            ctrl.busy = countPromise.then(dataPromiseOrNothing);
+          } else {
+            ctrl.busy = dataPromiseOrNothing(v4Params);
+          }
+
+        };
+
+      }
+
+      return {
+
+        setup: function (ctrl, model) {
+
+          var counts = !ctrl.ngTable.noPages && (ctrl.ngTable.counts || [12, 25, 50, 100]);
+          var count = ctrl.ngTable.count || 12;
+
+          if (counts.indexOf(count) < 0) {
+            counts.push(count);
+            counts = _.sortBy(counts);
+          }
+
+          ctrl.ngTableParams = new NgTableParams(angular.extend({
+            page: 1,
+            count: count,
+            clearData: function () {
+              lastFindAllData = [];
+            }
+          }, ctrl.ngTable), {
+            filterDelay: 0,
+            dataset: lastFindAllData,
+            counts: counts,
+            getData: getData (ctrl,model)
+          });
+
+          return ctrl.ngTableParams;
+        },
+
+        ngTableToV4Params: ngTableToV4Params
+
+      };
+
+    });
+
+}());
+
+(function () {
+
+  angular.module('sistemiumBootstrap.directives')
+    .directive('sabErrorWidget', function () {
+
+      return {
+
+        restrict: 'AC',
+        templateUrl: 'sistemium-angular-bootstrap/directives/sabErrorWidget/sabErrorWidget.html',
+        controllerAs: 'dm',
+
+        controller: function (sabErrorsService) {
+          var dm = this;
+          dm.errors =  sabErrorsService.errors;
+          dm.closeError = function (index) {
+            dm.errors.splice(index, 1);
+          };
+        }
+
+      };
+
+    });
+}());
+
 (function () {
 
   /**
@@ -147,29 +295,5 @@
 
 })();
 
-(function () {
-
-  angular.module('sistemiumBootstrap.directives')
-    .directive('sabErrorWidget', function () {
-
-      return {
-
-        restrict: 'AC',
-        templateUrl: 'sistemium-angular-bootstrap/directives/sabErrorWidget/sabErrorWidget.html',
-        controllerAs: 'dm',
-
-        controller: function (sabErrorsService) {
-          var dm = this;
-          dm.errors =  sabErrorsService.errors;
-          dm.closeError = function (index) {
-            dm.errors.splice(index, 1);
-          };
-        }
-
-      };
-
-    });
-}());
-
-(function(){angular.module("sistemiumBootstrap").run(["$templateCache", function($templateCache) {$templateCache.put("sistemium-angular-bootstrap/directives/sabInputWithAddon/sabInputWithAddon.html","<div class=\"form-group\"><div class=\"input-group\"><div uib-dropdown=\"uib-dropdown\" is-open=\"vm.isOpen\" class=\"input-group-btn\"><button type=\"button\" ng-class=\"vm.sabBtnClass\" uib-dropdown-toggle=\"uib-dropdown-toggle\" class=\"btn\">{{sabSelectModel[sabLabelProp]}} <span class=\"caret\"></span></button><ul class=\"dropdown-menu\"><li ng-repeat=\"item in sabSelectOptions\"><a href=\"\" ng-click=\"vm.setActiveItem(item)\">{{item[sabLabelProp]}}</a></li></ul></div><input ng-model=\"sabInputModel\" type=\"number\" ng-required=\"required\" class=\"form-control\"/></div></div>");
-$templateCache.put("sistemium-angular-bootstrap/directives/sabErrorWidget/sabErrorWidget.html","<div ng-show=\"dm.errors.length\"><uib-alert ng-repeat=\"error in dm.errors\" type=\"{{error.type}}\" close=\"dm.closeError($index)\">{{error.msg}}</uib-alert></div>");}]);})();
+(function(){angular.module("sistemiumBootstrap").run(["$templateCache", function($templateCache) {$templateCache.put("sistemium-angular-bootstrap/directives/sabErrorWidget/sabErrorWidget.html","<div ng-show=\"dm.errors.length\"><uib-alert ng-repeat=\"error in dm.errors\" type=\"{{error.type}}\" close=\"dm.closeError($index)\">{{error.msg}}</uib-alert></div>");
+$templateCache.put("sistemium-angular-bootstrap/directives/sabInputWithAddon/sabInputWithAddon.html","<div class=\"form-group\"><div class=\"input-group\"><div uib-dropdown=\"uib-dropdown\" is-open=\"vm.isOpen\" class=\"input-group-btn\"><button type=\"button\" ng-class=\"vm.sabBtnClass\" uib-dropdown-toggle=\"uib-dropdown-toggle\" class=\"btn\">{{sabSelectModel[sabLabelProp]}} <span class=\"caret\"></span></button><ul class=\"dropdown-menu\"><li ng-repeat=\"item in sabSelectOptions\"><a href=\"\" ng-click=\"vm.setActiveItem(item)\">{{item[sabLabelProp]}}</a></li></ul></div><input ng-model=\"sabInputModel\" type=\"number\" ng-required=\"required\" class=\"form-control\"/></div></div>");}]);})();
