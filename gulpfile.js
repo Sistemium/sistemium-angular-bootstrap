5fe4b7f186d200f99b50c0b669b7866d2cc70efb
Alexander Levin <alevin.ru@gmail.com>
2018-02-14 12:50:42 +0200

    sistemium-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,9 @@
-/**
- *  Welcome to your gulpfile!
- *  The gulp tasks are splitted in several files in the gulp directory
- *  because putting all here was really too long
- */
-
 'use strict';
 
-var gulp = require('gulp');
-var wrench = require('wrench');
-var runSequence = require('run-sequence');
-
-/**
- *  This will load all js or coffee files in the gulp directory
- *  in order to load all gulp tasks
- */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
-
-/**
- *  Default task clean temporaries directories and launch the
- *  main optimization build task
- */
-gulp.task('default', function () {
-  runSequence('clean', 'process-all', 'watch');
-});
-
-gulp.task('dist', function () {
-  runSequence('clean', 'process-all');
-});
+require('sistemium-gulp')
+  .config({
+    ngModule: 'sistemium',
+    tasks: 'lib_tasks',
+    concat: 'sistemium-angular-bootstrap.js'
+  })
+  .lib(require('gulp'));
