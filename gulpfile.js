8929dc2342da57dbd8a5df08588eec95e0a83bbc
Alexander Levin <alevin.ru@gmail.com>
2018-02-15 10:50:52 +0200

    sistemium-gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,29 @@
 
 require('sistemium-gulp')
   .config({
-    ngModule: 'sistemium',
+    ngModule: 'sistemiumBootstrap',
     tasks: 'lib_tasks',
-    concat: 'sistemium-angular-bootstrap.js'
+    concat: 'sistemium-angular-bootstrap.js',
+    styles: {
+      src: 'sistemium-angular-bootstrap',
+      index: 'index.scss',
+      concat: 'sistemium-angular-bootstrap.css',
+      replace: {
+        fonts: ['../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', 'fonts/bootstrap/']
+      }
+    },
+    others: {
+      sass: {
+        from: './src/sistemium-angular-bootstrap/**/*.scss',
+        to: '/sass',
+        replaces: [
+          ['src/sistemium-angular-bootstrap/styles/', ''],
+          ['../bower_components/bootstrap-sass/assets/fonts', '../bower_components/sistemium-angular-bootstrap/dist/fonts'],
+          ['bootstrap/_bootstrap.scss', '_bootstrap.scss'],
+          ['../../../bower_components/bootstrap-sass', '../../../bootstrap-sass']
+        ]
+      },
+      fonts: ['./bower_components/bootstrap-sass/assets/fonts/**/*.*', '/fonts/bootstrap']
+    }
   })
   .lib(require('gulp'));
